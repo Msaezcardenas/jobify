5595dffc613ef937ce688064204594bf46c3a54c
molu <msaezcardenas@gmail.com>
2024-01-08 14:29:07 -0300

    set up debounce function for a better user experience

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,13 +1,25 @@
-import { FormRow, FormRowSelect } from '.';
+import { FormRow, FormRowSelect, SubmitBtn } from './index.js';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { Form, useSubmit, Link } from 'react-router-dom';
 import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from '../../../utils/constants';
 import { useAllJobsContext } from '../pages/AllJobs';
-const SearchContainer = () => {
-  const { searchValues } = useAllJobsContext();
-  const { search, jobStatus, jobType, sort } = searchValues;
 
+const SearchContainer = () => {
+  const searchValues = useAllJobsContext();
+  console.log(searchValues);
+  const { search, jobType, jobStatus, sort } = searchValues;
   const submit = useSubmit();
+
+  const debounce = (onChange) => {
+    let timeout;
+    return (e) => {
+      const form = e.currentTarget.form;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        onChange(form);
+      }, 2000);
+    };
+  };
 
   return (
     <Wrapper>
@@ -15,14 +27,13 @@ const SearchContainer = () => {
         <h5 className='form-title'>search form</h5>
         <div className='form-center'>
           {/* search position */}
-
           <FormRow
             type='search'
             name='search'
             defaultValue={search}
-            onChange={(e) => {
-              submit(e.currentTarget.form);
-            }}
+            onChange={debounce((form) => {
+              submit(form);
+            })}
           />
           <FormRowSelect
             labelText='job status'
@@ -36,8 +47,8 @@ const SearchContainer = () => {
           <FormRowSelect
             labelText='job type'
             name='jobType'
-            defaultValue={jobType}
             list={['all', ...Object.values(JOB_TYPE)]}
+            defaultValue={jobType}
             onChange={(e) => {
               submit(e.currentTarget.form);
             }}
